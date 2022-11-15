93d7d2f7793691d3f8c19a19880e2453e5cf1529
Yongkang Lu <yongkanglu110@gmail.com>
2022-11-15 07:48:17 -0600

    final commit

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,33 @@
 import React from "react";
+import profilePircture from "../../../static/assets/images/bio/ministry-safe.jpg"
 
 export default function() {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+    <div 
+      className="left-column"
+      style={{
+        background:"url(" + profilePircture + ") no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    />
+    <div className="right-column">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc vel risus commodo viverra maecenas accumsan lacus. Faucibus a pellentesque sit amet porttitor eget. Sollicitudin tempor id eu nisl. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Diam maecenas sed enim ut sem viverra. Id faucibus nisl tincidunt eget. Eros donec ac odio tempor. A diam maecenas sed e
+      nim. Eget mauris pharetra et ultrices. Blandit aliquam etiam erat velit. Auctor eu augue ut lectus arcu. Eget lorem dolor
+      sed viverra ipsum nunc aliquet. Egestas quis ipsum suspendisse 
+      ultrices gravida. Justo donec enim diam vulputate ut pharetra. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Convallis aenean et tortor at risus viverra adipiscing at in.
+      Condimentum id venenatis a condimentum vitae sapi
+      pellentesque habitant morbi. Vel orci porta non pulvinar n
+      eque laoreet suspendisse interdum consectetur. Sed risus preti
+      um quam vulputate. Integer vitae justo eget magna fermentum iaculis eu non diam. Quis risus sed vulputate odio. Sollicitudin ac orci phasellus egestas tellus rutrum. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Nunc sed velit dignissim sodales ut. Nisl tincidunt eget nullam non nisi. Sit amet mauris commodo quis imperdiet mass
+      tincidunt nunc pulvinar. Convallis a cras semper auctor neque vitae tempus. Cursus turpis massa tincidunt dui ut. Vesti
+      bulum lorem sed risus ultricies. Cursus eget nunc scelerisque viverra mauris in a
+      liquam. Tortor vitae purus faucibus ornare suspendisse.
+
+    </div>
+    </div>
+
+  );
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,50 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePitcture from "../../../static/assets/images/auth/login.jpg"
 
 export default function() {
-  return <div>Contact</div>;
+  return (
+    <div className="content-page-wrapper">
+    <div 
+      className="left-column"
+      style={{
+        background:"url(" + contactPagePitcture + ") no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    />
+    <div className="right-column">
+      <div className="contact-bullet-points">
+        <div className="bullet-point-group">
+          <div className="icon">
+            <FontAwesomeIcon icon="phone"/>
+          </div>
+          <div className="text">
+            555-555-5555
+          </div>
+        </div>
+
+        <div className="bullet-point-group">
+          <div className="icon">
+            <FontAwesomeIcon icon="envelope"/>
+          </div>
+          <div className="text">
+            yongkanglu110@gmail.com
+          </div>
+        </div>
+
+        <div className="bullet-point-group">
+          <div className="icon">
+            <FontAwesomeIcon icon="map-marked-alt"/>
+          </div>
+          <div className="text">
+            Woodbury,MN
+          </div>
+        </div>
+      </div>
+
+    </div>
+    </div>
+
+  );
 }
